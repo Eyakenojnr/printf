70699c45feccaa5f517da9ab29fec5c04a374524
Uduak Umanah <eyakenojnr@gmail.com>
2022-05-22 18:39:50 -0700

    main

--- main.h
+++ main.h
@@ -34,17 +34,17 @@
  */
 typedef struct parameters
 {
-	unsigned int unsign        :1;
+	unsigned int unsign        : 1;
 	
-	unsigned int plus_flag     :1;
-	unsigned int space_flag    :1;
-	unsigned int hashtag_flag  :1;
-	unsigned int zero_flag     :1;
-	unsigned int minus_flag    :1;
+	unsigned int plus_flag     : 1;
+	unsigned int space_flag    : 1;
+	unsigned int hashtag_flag  : 1;
+	unsigned int zero_flag     : 1;
+	unsigned int minus_flag    : 1;
 
 	unsigned int width;
 	unsigned int precision;
 
-	unsigned int h_modifier    :1;
-	unsigned int l_modifier    :1;
+	unsigned int h_modifier    : 1;
+	unsigned int l_modifier    : 1;
 } params_t;
