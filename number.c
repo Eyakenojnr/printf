cd1911075bd6302290abd433f1d8a6e6c4fb04b4
Uduak Umanah <eyakenojnr@gmail.com>
2022-05-23 22:09:56 -0700

    number

---
+++ number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * *convert - converter function
+ *
+ * @num: number
+ * @base: base
+ * @flags: argument flags
+ * @params: parameter struct
+ *
+ * Return: string
+ */
+char *convert(long int num, int base, int flags, params_t *params)
+{
+	static char *array;
+	static char buffer[50];
+	char *ptr;
+	char sign = 0;
+	unsigned long n = num;
+	(void)params;
+
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do {
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
