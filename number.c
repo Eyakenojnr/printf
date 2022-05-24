56c2a9d4caf510c89df4827f66746dcad6a0e657
Joshua Alonge <alongejoshua@gmail.com>
2022-05-24 00:21:34 -0700

    number

--- number.c
+++ number.c
@@ -35,3 +35,45 @@ char *convert(long int num, int base, int flags, params_t *params)
 		*--ptr = sign;
 	return (ptr);
 }
+
+/**
+ * print_unsigned - prints unsigned integer numbers
+ * @ap: the pointer to argument
+ * @params: the struct parameters
+ *
+ * Return: bytes printed
+ */
+int print_unsigned(va_list ap, params_t *params)
+{
+	unsigned long l;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
+	params->unsign = 1;
+	return (print_number(convert(l, 10, CONVERT_UNSIGNED, params), params));
+}
+
+/**
+ * print_address - prints address
+ * @ap: the pointer to argument
+ * @params: the struct parameters
+ *
+ * Return: bytes printed
+ */
+int print_address(va_list ap, params_t *params)
+{
+	unsigned long int n = va_arg(ap, unsigned long int);
+	char *str;
+
+	if (!n)
+		return (_puts("(nil)"));
+
+	str = convert(n, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+	*--str = 'x';
+	*--str = '0';
+	return (print_number(str, params));
+}
