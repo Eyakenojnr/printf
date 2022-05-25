f8f1c050259cac507e5b0204a426a7834e4e2617
Uduak Umanah <eyakenojnr@gmail.com>
2022-05-25 03:34:26 -0700

    print different data types

--- print-functions.c
+++ print-functions.c
@@ -2,6 +2,7 @@
 
 /**
  * print_char - prints character
+ *
  * @ap: pointer to argument
  * @params: the struct parameters
  *
@@ -23,6 +24,7 @@ int print_char(va_lsit ap, params_t *params)
 
 /**
  * print_int - prints integer
+ *
  * @ap: pointer to argument
  * @params: the struct parameters
  *
@@ -44,6 +46,7 @@ int print_int(va_list ap, params_t *params)
 
 /**
  * print_string - prints string
+ *
  * @ap: argument pointer
  * @params: the struct parameters
  *
@@ -80,6 +83,54 @@ int print_string(va_list ap, params_t *params)
 				sum += _putchar(*str++);
 		else
 			sum += _puts(str);
+	}
+	return (sum);
+}
+
+/**
+ * print_percent - prints '%'
+ *
+ * @ap: argument pointer
+ * @params: the parameters struct
+ *
+ * Return: number chars printed
+ */
+int print_percent(va_list ap, params_t *params)
+{
+	(void)ap;
+	(void)params;
+	return (_putchar('%'));
+}
+
+/**
+ * print_S - custom format specifier
+ *
+ * @ap: argument pointer
+ * @params: the parameter struct
+ *
+ * Return: number chars printed
+ */
+int print_S(va_list ap, params_t *params)
+{
+	char *str = va_arg(ap, char *);
+	char *hex;
+	int sum = 0;
+
+	if ((int)(!str))
+		return (_puts(NULL_STRING));
+	for (; *str; str++)
+	{
+		if ((*str > 0 && *str < 32) || *str >= 127)
+		{
+			sum += _putchar('\\');
+			sum += _putchar('x');
+			hex = convert(*str, 16, 0, params);
+			if (!hex[1])
+				sum += _putchar('0');
+			sum += _puts(hex);
+		}
+		else
+			sum += _putchar(*str);
 	}
 	return (sum);
 }
