549aa88f622b0ed9e48bf7e1ab8d6c2a0dc85ecb
Joshua Alonge <alongejoshua@gmail.com>
2022-05-25 16:21:47 -0700

    simple printers

--- simple-printers.c
+++ simple-printers.c
@@ -41,8 +41,39 @@ int print_rev(va_list ap, params_t *params)
 		for (len = 0; *str; str++)
 			len++;
 		str--;
-		for(; len > 0; len--, str--)
+		for (; len > 0; len--, str--)
 			sum += _putchar(*str);
 	}
 	return (sum);
+}
+
+/**
+ * print_rot13 - prints string in rot13
+ * @ap: string
+ * @params: the struct parameters
+ *
+ * Return: number bytes printed
+ */
+int print_rot13(va_list ap, params_t *params)
+{
+	int i, index;
+	int count = 0;
+	char arr[] = "NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
+	char *a = va_arg(ap, char *);
+	(void)params;
+
+	i = 0;
+	index = 0;
+	while (a[i])
+	{
+		if ((a[i] >= 'A' && a[i] <= 'Z') || (a[i] >= 'a' && a[i] <= 'z'))
+		{
+			index = a[i] - 65;
+			count += _putchar(arr[index]);
+		}
+		else
+			count += _putchar(a[i]);
+		i++;
+	}
+	return (count);
 }
