587c8a52e44f5cdbd35e1cf634310bb2c7ff2dfe
Uduak Umanah <eyakenojnr@gmail.com>
2022-05-25 04:26:14 -0700

    identify specifiers

--- specifier.c
+++ specifier.c
@@ -54,3 +54,87 @@ int get_print_func(char *s, va_list ap, params_t *params)
 		return (f(ap, params));
 	return (0);
 }
+
+/**
+ * get_flag - function that finds the flag function
+ *
+ * @s: format string
+ * @params: the parameters struct
+ *
+ * Return: if flag was valid
+ */
+int get_flag(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case '+':
+			i = params->plus_flag = 1;
+			break;
+		case ' ':
+			i = params->space_flag = 1;
+			break;
+		case '#':
+			i = params->hashtag_flag = 1;
+			break;
+		case '-':
+			i = params->minus_flag = 1;
+			break;
+		case '0':
+			i = params->zero_flag = 1;
+			break;
+	}
+	return (i);
+}
+
+/**
+ * get_modifier - function that finds the modifier function
+ *
+ * @s: the format string
+ * @params: the parameters struct
+ *
+ * Return: if modifier is valid
+ */
+int get_modifier(char *s, params_t *params)
+{
+	int i = 0;
+
+	switch (*s)
+	{
+		case 'h':
+			i = params->h_modifier = 1;
+			break;
+		case 'l':
+			i = params->l_modifier = 1;
+			break;
+	}
+	return (i);
+}
+
+/**
+ * *get_width - function that gets the width from the format string
+ *
+ * @s: the format string
+ * @params: the parameters struct
+ * @ap: the argument pointer
+ *
+ * Return: new pointer
+ */
+char *get_width(char *s, params_t *params, va_list ap)
+{
+	int d = 0;
+
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+	params->width = d;
+	return (s);
+}
