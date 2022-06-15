03482c8388122ab1b2979dd255dd29e73c864a64
Uduak Umanah <eyakenojnr@gmail.com>
2022-06-15 05:32:19 -0700

    prints character bytes at a time

--- _put.c
+++ _put.c
@@ -17,17 +17,17 @@ int _puts(char *str)
 
 /**
  * _putchar - writes the character c to stdout
- * @c: character
+ * @c: The character to print
  *
- * Return: 1 on success, -1 on failure
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(int c)
 {
 	static int i;
 	static char buf[OUTPUT_BUF_SIZE];
 
-	if (c == BUF_FLUSH || I >= OUTPUT_BUF_SIZE)
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
 	{
 		write(1, buf, i);
 		i = 0;
